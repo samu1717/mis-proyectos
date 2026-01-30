ddc354b76bbea0c0c2f38c5b234b278d2500c932
achraf <pythonprueba@gmail.com>
2026-01-30 17:41:22 +0100

    Explico el cambio

--- dados.py
+++ dados.py
@@ -1,23 +1,42 @@
-#Juego de dados entre dos jugadores
 import random
-suma=0
-suma2=0
-for jugador1 in range (6):
-    aleatorio=random.randint(1,6)
-    suma+=aleatorio
-    print("El valor del dado es:",aleatorio)
-print("La suma de los dados es jugador1 es :",suma)
-for jugador2 in range (6):
-    aleatorio=random.randint(1,6)
-    suma2+=aleatorio
-    print("El valor del dado es:",aleatorio)
-print("La suma de los dados es del jugador2 es :",suma2)
-if suma>suma2:
-    print("Gana el jugador 1")
-elif suma==suma2:
-    print("Empate")
-else:
-    print("Gana el jugador 2")
+
+print("Pulsa Enter para tirar los dados de cada jugador. Ctrl+C para terminar el juego.")
+
+for ronda in range(1000000000):  
+
+    suma = 0
+    suma2 = 0
+
+    
+    input("Jugador 1, pulsa Enter para tirar los dados...")
+    for jugador1 in range(6):  
+        aleatorio = random.randint(1, 6)
+        suma += aleatorio
+        print("El valor del dado es:", aleatorio)
+    print("La suma de los dados del jugador 1 es:", suma)
+
+    # Turno del Jugador 2
+    input("Jugador 2, pulsa Enter para tirar los dados...")
+    for jugador2 in range(6):  
+        aleatorio = random.randint(1, 6)
+        suma2 += aleatorio
+        print("El valor del dado es:", aleatorio)
+    print("La suma de los dados del jugador 2 es:", suma2)
+
+    if suma > suma2:
+        print("Gana el jugador 1")
+    elif suma == suma2:
+        print("Empate")
+    else:
+        print("Gana el jugador 2")
+
+    print("----------------------------")
+
+
+
+
+
+
 
 
 
